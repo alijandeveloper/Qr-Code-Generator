f82433ea50ce1a4fc8726471eaa02be373afbc74
Ali Jan <alijandeveloper15@gmail.com>
2025-03-05 12:27:42 -0800

    getElement in js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const textInput = document.getElementById("textInput");
+    const generateBtn = document.getElementById("generateBtn");
+    const qrContainer = document.getElementById("qrContainer");
+    const downloadBtn = document.getElementById("downloadBtn");
+    const scanBtn = document.getElementById("scanBtn");
+    const toggleMode = document.getElementById("toggleMode");
+    const historyList = document.getElementById("historyList");
